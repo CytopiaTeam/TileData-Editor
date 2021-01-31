cbba834835fabbe5fc1c79b2ec67f36b4ad6ce66
SimplyLiz <talantyyr@gmail.com>
2021-01-31 16:17:13 +0100

    revert shoretiles removal

--- src/TileDataContainer.cxx
+++ src/TileDataContainer.cxx
@@ -73,6 +73,7 @@ QString TileDataContainer::loadFile(const QString& theFileName)
 		tileTypeFromJson(tile.tileType, obj.value("tileType"));
 
 		tileSetDataFromJson(tile.tiles, obj.value("tiles"));
+		tileSetDataFromJson(tile.shoreTiles, obj.value("shoreTiles"));
 		tileSetDataFromJson(tile.slopeTiles, obj.value("slopeTiles"));
 
 		tileData.insert(id, tile);
@@ -189,6 +190,9 @@ bool TileDataContainer::saveFile()
 
 		if (!tile.tiles.fileName.empty())
 			obj.insert("tiles", tileSetDataToJson(tile.tiles));
+
+		if (!tile.shoreTiles.fileName.empty())
+			obj.insert("shoreTiles", tileSetDataToJson(tile.shoreTiles));
 
 		if (!tile.slopeTiles.fileName.empty())
 			obj.insert("slopeTiles", tileSetDataToJson(tile.slopeTiles));
